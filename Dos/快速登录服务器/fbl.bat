c1b8c0096269becf90f62a1776d3402c2fa0437e
fblrainbow <846058904@qq.com>
2018-09-03 00:35:24 +0800

    [Commit]       :增加快速登录服务器的工具 [Modify]       :范伟平 [Code Review]  :范炳林

---
+++ Dos/快速登录服务器/fbl.bat
@@ -0,0 +1,9 @@
+@echo off
+
+FOR /F "eol=; tokens=1,2 delims==" %%i in (fbl.ini) do (
+if %%i==IP set IP=%%j
+if %%i==Username set Username=%%j
+if %%i==Password set Password=%%j
+)
+echo %IP% - %Username% - %Password%
+START putty %Username%@%IP% -pw %Password%
